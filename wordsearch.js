8f6a30bcd4099f29a9712945206f18ba90e7f703
joycuicui <cuiqijoy@outlook.com>
2023-11-22 20:41:37 -0800

    fix code and add more test cases

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) {
+    return false;
+  }
 
-module.exports = wordSearch
+  for (const array of letters) {
+    const horizontalJoin = array.join("");
+    if (horizontalJoin.includes(word.toUpperCase())) {
+      return true;
+    }
+  }
+  for (let i = 0; i < letters[0].length; i++) {
+    let vertical = "";
+    for (let j = 0; j < letters.length; j++) {
+      vertical += letters[j][i];
+    }
+    if (vertical.includes(word.toUpperCase())) {
+      return true;
+    }
+  }
+  return false;
+};
+
+module.exports = wordSearch;
